467f69a909c89c81f4251cea510c6477292f8054
Makar Baderko <makarbaderko@gmail.com>
2022-06-28 15:16:31 +0300

    Reached some point at debugging contest n6

--- lec_6/B6.cpp
+++ lec_6/B6.cpp
@@ -1,3 +1,9 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+
 struct Node {
   int left, right;
   int value;
@@ -8,15 +14,20 @@ struct Node {
   int assigned;
   int add;
   bool isReversed;
-}
+};
 
-Node nodes[MAXN];
+Node nodes[1000];
 int nodesNumber = 0;
+
+int getRand(){
+  return (rand() << 15) + rand();
+}
 
 int buildNewNode(int value){
   Node newNode;
   newNode.left = -1;
   newNode.right = -1;
+  newNode.value = value;
   newNode.pr = getRand();
   newNode.sz = 1;
   newNode.sum = value;
@@ -35,6 +46,10 @@ int &left(int v){
 int &right(int v){
   return nodes[v].right;
 }
+int &pr(int v){
+  return nodes[v].pr;
+}
+
 
 int &sum(int v){
   return nodes[v].sum;
@@ -43,6 +58,28 @@ int &sum(int v){
 int &sz(int v){
   return nodes[v].sz;
 }
+
+int &add(int v){
+  return nodes[v].add;
+}
+
+bool &isAssigned(int v){
+  return nodes[v].isAssigned;
+}
+
+int &assigned(int v){
+  return nodes[v].assigned;
+}
+
+int &value(int v){
+  return nodes[v].value;
+}
+
+ bool &isReversed(int v){
+  return nodes[v].isReversed;
+}
+
+
 
 int getSum(int v){
   return (v >= 0 ? sum(v) : 0);
@@ -61,7 +98,7 @@ pair<int, int> splitBySize(int root, int szLeft) {
   // Разбить дерево на 2 поддерева так,
   //чтобы размер левого поддерева был рвен szLeft
   if (szLeft == 0) {
-    return make_pair(-1; root);
+    return make_pair(-1, root);
   } if (getSz(left(root)) >= szLeft){
     push(root);
     pair<int, int>p = splitBySize(left(root), szLeft);
@@ -73,35 +110,18 @@ pair<int, int> splitBySize(int root, int szLeft) {
   }
 }
 
-int merge(int root1, int root2) {
-  if (root1 < 0){
-    return root2
-  } if (root2 < 0) {
-    return root1
-  } if (pr(root1) < pr(root2)) {
-    push(root1);
-    right(root1) = merge(right(root1), root2);
-    recalculate(root1);
-    return root1;
-  } else {
-    //Самостоятельное написание
-  }
-}
-
-int insert(int root, int pos, int x){
-    pair<int, int>p = splitBySize(root, pos - 1);
-    int newV = buildNewNode(x);
-    return merge(merge(p.first, newv), p.second);
-}
-
 struct Triple {
   int left, middle, right;
-}
+};
 
 Triple getSubsegment(int root, int l, int r) {
   pair<int, int>p = splitBySize(root, l - 1);
   pair<int, int>p2 =  splitBySize(p.second, r - l + 1);
-  return Triple{p.first, p2.first, p2.second};
+  Triple newTriple;
+  newTriple.left = p.first;
+  newTriple.middle = p2.first;
+  newTriple.right = p2.second;
+  return newTriple;
 }
 
 int mergeTriple(Triple t){
@@ -138,11 +158,11 @@ void assignOnSubtree(int root, int x){
 
 void addOnSubtree(int root, int x){
   if (isAssigned(root)){
-    assigned(root) += x
+    assigned(root) += x;
   } else {
     add(root) += x;
-  } value[root] += x;
-  sum[root] += x * sz(root);
+  } value(root) += x;
+  sum(root) += x * sz(root);
 }
 
 void reverseOnSubtree(int root) {
@@ -151,6 +171,14 @@ void reverseOnSubtree(int root) {
   }
   swap(left(root), right(root));
   isReversed(root) ^= 1;
+}
+
+
+
+void assignOnSubsegment(int root, int l, int r, int x) {
+  Triple t = getSubsegment(root, l, r);
+  assignOnSubtree(t.middle, x);
+  root = mergeTriple(t);
 }
 
 void push(int root){
@@ -172,8 +200,23 @@ void push(int root){
   }
 }
 
-void assignOnSubsegment(int root, int l, int r, int x) {
-  Triple t = getSubsegment(root, l, r);
-  assignOnSubtree(t.middle, x);
-  root = mergeTriple(t);
+int merge(int root1, int root2) {
+  if (root1 < 0){
+    return root2;
+  } if (root2 < 0) {
+    return root1;
+  } if (pr(root1) < pr(root2)) {
+    push(root1);
+    right(root1) = merge(right(root1), root2);
+    recalculate(root1);
+    return root1;
+  } else {
+    //Самостоятельное написание
+  }
+}
+
+int insert(int root, int pos, int x){
+    pair<int, int>p = splitBySize(root, pos - 1);
+    int newV = buildNewNode(x);
+    return merge(merge(p.first, newV), p.second);
 }
